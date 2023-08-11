6f048cf2648e6591b504303c92dc0dd8e6d51ad1
aravind.sridhar <aravind.sridhar@conxai.com>
2023-08-12 00:51:48 +0530

    comments for reportView; searchView

--- api/views/reportView.py
+++ api/views/reportView.py
@@ -14,36 +14,39 @@ from ..serializers import ReportSerializer, ReportListSerializer, BalanceSheetSe
 
 from ..scripts.report import upload_pdf_to_s3
 
-
 class ReportStatusView(APIView):
+    # Allow unauthenticated access for checking report status
     permission_classes = [AllowAny]
 
     def get(self, request):
+        # Extract request ID from query parameters
         request_id = request.query_params.get('request_id', None)
 
         if request_id is None:
             raise BadRequest('Request ID is missing.')
         
+        # Retrieve report object from the database
         report = Report.objects.get(id=request_id)
         reportData = ReportListSerializer(report).data
 
         return Response(reportData, status=status.HTTP_200_OK)
 
-
 class ReportInitiateView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
+        # Extract company symbol from query parameters
         company = request.query_params.get('company', None)
 
         if company is None:
             raise BadRequest('Company is missing.')
         
+        # Create an report object with a unique request_id
         serializer = ReportSerializer(data={})
         serializer.is_valid(raise_exception=True)
         report = serializer.save()
 
-        # Spawn a thread to execute asynchronously
+        # Spawn a thread to execute the report generation asynchronously
         thread_balance_sheet = threading.Thread(target=report_generation, args=(report.id, company))
         thread_balance_sheet.start()
 
@@ -51,33 +54,38 @@ class ReportInitiateView(APIView):
             "request_id": report.id,
             "status": report.status
         }, status=status.HTTP_201_CREATED)
-    
 
 def report_generation(report_id, company):
+    # Get API configurations from settings
     apikey = settings.ALPHA_API_KEY
     bs_base_url = settings.ALPHA_BALANCE_SHEET_URL
     bs_function = settings.ALPHA_BALANCE_SHEET_FUNCTION
     news_base_url = settings.ALPHA_NEWS_URL
     news_function = settings.ALPHA_NEWS_FUNCTION
 
+    # Set parameters for balance sheet API
     bs_params = {
         'function': bs_function,
         'symbol': company,
         'apikey': apikey
     }
 
+    # Make a request to the balance sheet API
     bs_response = requests.get(bs_base_url, params=bs_params)
     balanceSheet = bs_response.json()
 
+    # Set parameters for news API
     news_params = {
         'function': news_function,
         'symbol': company,
         'apikey': apikey
     }
 
+    # Make a request to the news API
     news_response = requests.get(news_base_url, params=news_params)
     news = news_response.json()
 
+    # Save balance sheet data to the database
     serializer = BalanceSheetSerializer(data={
         "data": balanceSheet,
         "report": report_id
@@ -85,10 +93,7 @@ def report_generation(report_id, company):
     serializer.is_valid(raise_exception=True)
     serializer.save()
 
+    # Upload the generated PDF (including balance sheet data and news data) report to S3
     upload_pdf_to_s3(balanceSheet, news, report_id)
 
     return
-
-
-
-
--- api/views/searchView.py
+++ api/views/searchView.py
@@ -10,37 +10,50 @@ import requests
 
 
 class SearchView(APIView):
+    # Allow unauthenticated access for company search
     permission_classes = [AllowAny]
     
     def get(self, request):
+        # Extract search query from query parameters
         query = request.query_params.get('q', None)
 
         if query is None:
             raise BadRequest('Query parameter "q" is missing.')
         
+        # Get API configurations from settings
         base_url = settings.ALPHA_SEARCH_URL
         function = settings.ALPHA_SEARCH_FUNCTION
         apikey = settings.ALPHA_API_KEY
 
+        # Set parameters for the search API request
         params = {
             'function': function,
             'keywords': query,
             'apikey': apikey
         }
 
+        # Make a request to the search API
         response = requests.get(base_url, params=params)
         
+        # format and return response data if response is successful
         if response.status_code == 200:
             result_key = 'bestMatches'
             formatted_data = self.format_response_data(response.json().get(result_key))
             return Response({result_key: formatted_data}, status=status.HTTP_200_OK)
         
+        # Return an error response if the API request fails
         return Response({'error': 'An error occurred while fetching data.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def format_response_data(self, list_of_dicts):
+        """
+        Formats the response data received from the AlphaVantage API.
+
+        :param list_of_dicts: List of dictionaries containing raw API response data.
+        :return: Formatted list of dictionaries with relevant information.
+        """
         formatted_data = []
         for entry in list_of_dicts:
-            # explicit creation for better performance, as opposed to dynamically going through the keys
+            # explicit creation for better performance, as opposed to dynamically altering the keys
             formatted_entry = {
                 'symbol': entry.get('1. symbol'),
                 'name': entry.get('2. name'),
@@ -54,4 +67,3 @@ class SearchView(APIView):
             }
             formatted_data.append(formatted_entry)
         return formatted_data
-    
